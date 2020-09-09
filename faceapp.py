ef6e9716acfc6df249673945acd10630f5170039
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2020-09-09 12:01:38 +0530

    Update faceapp.py

--- faceapp.py
+++ faceapp.py
@@ -1,14 +1,5 @@
 import cv2
-# import matplotlib.pyplot as plt
 
-# capture=cv2.VideoCapture(0) #To open camera 1
-# capture=cv2.imread('girl.png')
-# if (capture.isOpened()==False):
-# 	print("Sorry")
-# while(capture.isOpened()):
-
-    # Load trained cascade classifier
-# ret,frame=capture.read()
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
  
@@ -18,8 +9,6 @@ color_image = cv2.imread('image.jpg')
  
     # Convert color image into grayscale
 gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
- 
-    # Detect faces ROI
     #Syntax: Classifier.detectMultiScale(input image, Scale Factor , Min Neighbors)
 faces = face_cascade.detectMultiScale(gray_image, 1.1, 5) 
     # print(faces)
@@ -27,7 +16,7 @@ eyes=eye_cascade.detectMultiScale(gray_image, 1.1, 5)
     # Draw rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 255), 4)
-        # print(x,y,w,h)
+    # Draw rectangle around the eyes
 for (x, y, w, h) in eyes:
     cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
      
@@ -35,10 +24,4 @@ for (x, y, w, h) in eyes:
 cv2.imshow('Image', color_image)
 cv2.imwrite('detect.png',color_image)
 cv2.waitKey(3000)
-# color_image.release()
 cv2.destroyAllWindows()
-# pics=['multi2.png','girl.png']
-# import random
-# detect_pic(random.choice(pics))
-# import pickle
-# pickle.dump(detect_pic('multi2.png'),open('model.pkl','w'))
